074427caec921311b731b24f94f2b09ab14e1578
Dylan Meeus <meeusdylan@protonmail.com>
2017-03-13 15:34:55 +0100

    HTML headers, ordered and unordered lists

--- BlocklyDCG/src/blockly/blocks/html.js
+++ BlocklyDCG/src/blockly/blocks/html.js
@@ -250,6 +250,290 @@ Blockly.Blocks['html_underline'] = {
 };
 
 
+
+Blockly.Blocks['html_header'] = {
+    /**
+     * Block for comparison operator.
+     * @this Blockly.Block
+     */
+    init: function() {
+
+        var HEADER_LEVEL = [ // supported header levels (h1,h2,h3,..)
+            ['1', '1'],
+            ['2', '2'],
+            ['3', '3'],
+            ['4', '4'],
+        ];
+        this.setHelpUrl(Blockly.Msg.LOGIC_COMPARE_HELPURL);
+        this.setColour(Blockly.Blocks.html.HUE);
+        this.setOutput(true, 'String');
+        this.appendDummyInput()
+            .appendField("Hoofding");
+        this.appendValueInput('A') // first input section
+            .appendField(new Blockly.FieldDropdown(HEADER_LEVEL), 'LVL');
+        this.setInputsInline(true);
+        // Assign 'this' to a variable for use in the tooltip closure below.
+        var thisBlock = this;
+        this.setTooltip("tooltip")
+        this.prevBlocks_ = [null, null];
+    }
+};
+
+
+Blockly.Blocks['html_list_numbered'] = {
+    /**
+     * Block for creating a list with any number of elements of any type.
+     * @this Blockly.Block
+     */
+    init: function() {
+        this.setHelpUrl(Blockly.Msg.LISTS_CREATE_WITH_HELPURL);
+        this.setColour(Blockly.Blocks.html.HUE);
+        this.itemCount_ = 3;
+        this.updateShape_();
+        this.setOutput(true, 'Array');
+        this.setMutator(new Blockly.Mutator(['html_block']));
+        this.setTooltip(Blockly.Msg.LISTS_CREATE_WITH_TOOLTIP);
+    },
+    /**
+     * Create XML to represent list inputs.
+     * @return {!Element} XML storage element.
+     * @this Blockly.Block
+     */
+    mutationToDom: function() {
+        var container = document.createElement('mutation');
+        container.setAttribute('items', this.itemCount_);
+        return container;
+    },
+    /**
+     * Parse XML to restore the list inputs.
+     * @param {!Element} xmlElement XML storage element.
+     * @this Blockly.Block
+     */
+    domToMutation: function(xmlElement) {
+        this.itemCount_ = parseInt(xmlElement.getAttribute('items'), 10);
+        this.updateShape_();
+    },
+    /**
+     * Populate the mutator's dialog with this block's components.
+     * @param {!Blockly.Workspace} workspace Mutator's workspace.
+     * @return {!Blockly.Block} Root block in mutator.
+     * @this Blockly.Block
+     */
+    decompose: function(workspace) {
+        var containerBlock = workspace.newBlock('html_blocks_container');
+        containerBlock.initSvg();
+        var connection = containerBlock.getInput('STACK').connection;
+        for (var i = 0; i < this.itemCount_; i++) {
+            var itemBlock = workspace.newBlock('html_block');
+            itemBlock.initSvg();
+            connection.connect(itemBlock.previousConnection);
+            connection = itemBlock.nextConnection;
+        }
+        return containerBlock;
+    },
+    /**
+     * Reconfigure this block based on the mutator dialog's components.
+     * @param {!Blockly.Block} containerBlock Root block in mutator.
+     * @this Blockly.Block
+     */
+    compose: function(containerBlock) {
+        var itemBlock = containerBlock.getInputTargetBlock('STACK');
+        // Count number of inputs.
+        var connections = [];
+        while (itemBlock) {
+            connections.push(itemBlock.valueConnection_);
+            itemBlock = itemBlock.nextConnection &&
+                itemBlock.nextConnection.targetBlock();
+        }
+        // Disconnect any children that don't belong.
+        for (var i = 0; i < this.itemCount_; i++) {
+            var connection = this.getInput('ADD' + i).connection.targetConnection;
+            if (connection && connections.indexOf(connection) == -1) {
+                connection.disconnect();
+            }
+        }
+        this.itemCount_ = connections.length;
+        this.updateShape_();
+        // Reconnect any child blocks.
+        for (var i = 0; i < this.itemCount_; i++) {
+            Blockly.Mutator.reconnect(connections[i], this, 'ADD' + i);
+        }
+    },
+    /**
+     * Store pointers to any connected child blocks.
+     * @param {!Blockly.Block} containerBlock Root block in mutator.
+     * @this Blockly.Block
+     */
+    saveConnections: function(containerBlock) {
+        var itemBlock = containerBlock.getInputTargetBlock('STACK');
+        var i = 0;
+        while (itemBlock) {
+            var input = this.getInput('ADD' + i);
+            itemBlock.valueConnection_ = input && input.connection.targetConnection;
+            i++;
+            itemBlock = itemBlock.nextConnection &&
+                itemBlock.nextConnection.targetBlock();
+        }
+    },
+    /**
+     * Modify this block to have the correct number of inputs.
+     * @private
+     * @this Blockly.Block
+     */
+    updateShape_: function() {
+        if (this.itemCount_ && this.getInput('EMPTY')) {
+            this.removeInput('EMPTY');
+        } else if (!this.itemCount_ && !this.getInput('EMPTY')) {
+            this.appendDummyInput('EMPTY')
+                .appendField("Voeg een element toe.");
+        }
+        // Add new inputs.
+        for (var i = 0; i < this.itemCount_; i++) {
+            if (!this.getInput('ADD' + i)) {
+                var input = this.appendValueInput('ADD' + i);
+                if (i == 0) {
+                    input.appendField("Genummerde Lijst");
+                }
+            }
+        }
+        // Remove deleted inputs.
+        while (this.getInput('ADD' + i)) {
+            this.removeInput('ADD' + i);
+            i++;
+        }
+    }
+};
+
+
+/**
+ * Block for unordered lists in HTML
+ * @type {{init: Blockly.Blocks.html_list_numbered.init, mutationToDom: Blockly.Blocks.html_list_numbered.mutationToDom, domToMutation: Blockly.Blocks.html_list_numbered.domToMutation, decompose: Blockly.Blocks.html_list_numbered.decompose, compose: Blockly.Blocks.html_list_numbered.compose, saveConnections: Blockly.Blocks.html_list_numbered.saveConnections, updateShape_: Blockly.Blocks.html_list_numbered.updateShape_}}
+ */
+Blockly.Blocks['html_list_unnumbered'] = {
+    /**
+     * Block for creating a list with any number of elements of any type.
+     * @this Blockly.Block
+     */
+    init: function() {
+        this.setHelpUrl(Blockly.Msg.LISTS_CREATE_WITH_HELPURL);
+        this.setColour(Blockly.Blocks.html.HUE);
+        this.itemCount_ = 3;
+        this.updateShape_();
+        this.setOutput(true, 'Array');
+        this.setMutator(new Blockly.Mutator(['html_block']));
+        this.setTooltip(Blockly.Msg.LISTS_CREATE_WITH_TOOLTIP);
+    },
+    /**
+     * Create XML to represent list inputs.
+     * @return {!Element} XML storage element.
+     * @this Blockly.Block
+     */
+    mutationToDom: function() {
+        var container = document.createElement('mutation');
+        container.setAttribute('items', this.itemCount_);
+        return container;
+    },
+    /**
+     * Parse XML to restore the list inputs.
+     * @param {!Element} xmlElement XML storage element.
+     * @this Blockly.Block
+     */
+    domToMutation: function(xmlElement) {
+        this.itemCount_ = parseInt(xmlElement.getAttribute('items'), 10);
+        this.updateShape_();
+    },
+    /**
+     * Populate the mutator's dialog with this block's components.
+     * @param {!Blockly.Workspace} workspace Mutator's workspace.
+     * @return {!Blockly.Block} Root block in mutator.
+     * @this Blockly.Block
+     */
+    decompose: function(workspace) {
+        var containerBlock = workspace.newBlock('html_blocks_container');
+        containerBlock.initSvg();
+        var connection = containerBlock.getInput('STACK').connection;
+        for (var i = 0; i < this.itemCount_; i++) {
+            var itemBlock = workspace.newBlock('html_block');
+            itemBlock.initSvg();
+            connection.connect(itemBlock.previousConnection);
+            connection = itemBlock.nextConnection;
+        }
+        return containerBlock;
+    },
+    /**
+     * Reconfigure this block based on the mutator dialog's components.
+     * @param {!Blockly.Block} containerBlock Root block in mutator.
+     * @this Blockly.Block
+     */
+    compose: function(containerBlock) {
+        var itemBlock = containerBlock.getInputTargetBlock('STACK');
+        // Count number of inputs.
+        var connections = [];
+        while (itemBlock) {
+            connections.push(itemBlock.valueConnection_);
+            itemBlock = itemBlock.nextConnection &&
+                itemBlock.nextConnection.targetBlock();
+        }
+        // Disconnect any children that don't belong.
+        for (var i = 0; i < this.itemCount_; i++) {
+            var connection = this.getInput('ADD' + i).connection.targetConnection;
+            if (connection && connections.indexOf(connection) == -1) {
+                connection.disconnect();
+            }
+        }
+        this.itemCount_ = connections.length;
+        this.updateShape_();
+        // Reconnect any child blocks.
+        for (var i = 0; i < this.itemCount_; i++) {
+            Blockly.Mutator.reconnect(connections[i], this, 'ADD' + i);
+        }
+    },
+    /**
+     * Store pointers to any connected child blocks.
+     * @param {!Blockly.Block} containerBlock Root block in mutator.
+     * @this Blockly.Block
+     */
+    saveConnections: function(containerBlock) {
+        var itemBlock = containerBlock.getInputTargetBlock('STACK');
+        var i = 0;
+        while (itemBlock) {
+            var input = this.getInput('ADD' + i);
+            itemBlock.valueConnection_ = input && input.connection.targetConnection;
+            i++;
+            itemBlock = itemBlock.nextConnection &&
+                itemBlock.nextConnection.targetBlock();
+        }
+    },
+    /**
+     * Modify this block to have the correct number of inputs.
+     * @private
+     * @this Blockly.Block
+     */
+    updateShape_: function() {
+        if (this.itemCount_ && this.getInput('EMPTY')) {
+            this.removeInput('EMPTY');
+        } else if (!this.itemCount_ && !this.getInput('EMPTY')) {
+            this.appendDummyInput('EMPTY')
+                .appendField("Voeg een element toe.");
+        }
+        // Add new inputs.
+        for (var i = 0; i < this.itemCount_; i++) {
+            if (!this.getInput('ADD' + i)) {
+                var input = this.appendValueInput('ADD' + i);
+                if (i == 0) {
+                    input.appendField("Ongenummerde Lijst");
+                }
+            }
+        }
+        // Remove deleted inputs.
+        while (this.getInput('ADD' + i)) {
+            this.removeInput('ADD' + i);
+            i++;
+        }
+    }
+};
+
+
 /**
  * HTML-linebreak
  * @type {{init: Blockly.Blocks.html_break.init}}
--- BlocklyDCG/src/blockly/generators/dcg/html.js
+++ BlocklyDCG/src/blockly/generators/dcg/html.js
@@ -62,6 +62,57 @@ Blockly.Dcg['html_underline'] = function(block) {
     return ['<u>' + withoutQuotes + '</u>',Blockly.Dcg.ORDER_NONE];
 };
 
+
+/**
+ * Generator for the HTML header
+ * @param block
+ * @returns {*[]}
+ */
+Blockly.Dcg['html_header'] = function(block) {
+    // Operations 'and', 'or'.
+    var header_level = (block.getFieldValue('LVL'));
+    var argument0 = Blockly.Dcg.valueToCode(block, 'A', Blockly.Dcg.ORDER_NONE);
+    argument0 = argument0.replace(/'/g, "");
+    var code = '<h' + header_level + '>' + argument0 + '</h' + header_level + '>'
+    return [code, Blockly.Dcg.ORDER_NONE];
+};
+
+
+/**
+ * Generator to make a numbered list.
+ * @param block
+ * @returns {*[]}
+ */
+Blockly.Dcg['html_list_numbered'] = function(block) {
+    // Create a list with any number of elements of any type.
+    var elements = new Array(block.itemCount_);
+    for (var i = 0; i < block.itemCount_; i++) {
+        elements[i] = Blockly.Dcg.valueToCode(block, 'ADD' + i,
+                Blockly.Dcg.ORDER_NONE) || 'None';
+    }
+    var liWrappedElements = elements.map(x => x = "<li>" + x + "</li>");
+    var code = '<ol>' + liWrappedElements.join('') + '</ol>';
+    return [code, Blockly.Dcg.ORDER_ATOMIC];
+};
+/**
+ *
+ * Generator to make an unnumbered list.
+ * @param block
+ * @returns {*[]}
+ */
+Blockly.Dcg['html_list_unnumbered'] = function(block) {
+    // Create a list with any number of elements of any type.
+    var elements = new Array(block.itemCount_);
+    for (var i = 0; i < block.itemCount_; i++) {
+        elements[i] = Blockly.Dcg.valueToCode(block, 'ADD' + i,
+                Blockly.Dcg.ORDER_NONE) || 'None';
+    }
+    var liWrappedElements = elements.map(x => x = "<li>" + x + "</li>");
+    var code = '<ul>' + liWrappedElements.join('') + '</ul>';
+    return [code, Blockly.Dcg.ORDER_ATOMIC];
+};
+
+
 Blockly.Dcg['html_break'] = function(block) {
     return ['</br>',Blockly.Dcg.ORDER_NONE];
 };
